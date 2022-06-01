c030dc83f4bca4bd82e2dfb8221a42fbda87c215
CarlosB45 <carlosbarriosm47@gmail.com>
2022-05-31 23:07:57 -0500

    Create Home.jsx

---
+++ prueba/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/Styles.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function Home() {
+    return(
+        <div></div>
+    );
+}
+
+export default Home;
